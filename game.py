b53fbd0558004063b2ae34012a63e9c853971eaa
pgenev <philip_genev@abv.bg>
2016-12-02 18:30:03 +0200

    progress

--- game.py
+++ game.py
@@ -1,12 +1,11 @@
 import random
 
 
-GUESSED_WORDS = []
-
 class Game:
 
 	def __init__(self):
-		pass
+		self.current_word = ""
+		self.pattern = []
 
 	def display_menu(self):
 
@@ -28,35 +27,89 @@ class Game:
 				print ("\nPlease enter a valid choice!\n")
 				continue
 			elif (choice == 1):
-				self.load_random_word()
+				self.start_game()
 				return
 			elif (choice == 2):
 				break
 	
 	def load_random_word(self):
 
-		dictionary = 'dictionary1.dat'
+		dictionary = 'dictionary.dat'
 
 		try:
 			with open(dictionary, 'r') as fp:
 				all_words = fp.readlines()
+				all_words = [word[:-1] for word in all_words]
 				random_word = all_words[random.randint(0, len(all_words)-1)]
 				if (len(random_word) < 4):
 					while(True):
 						random_word = all_words[random.randint(0, len(all_words)-1)]
-						if ((random_word in GUESSED_WORDS) or (len(random_word) < 4)):
+						if ((self.check_word_in_guessed_words(random_word) == True) or (len(random_word) < 4)):
 							random_word = all_words[random.randint(0, len(all_words)-1)]
 							continue
 						break
-
-				print (random_word)
+				return random_word
 		except IOError:
 			print ("\nCould not open the file {}\n".format(dictionary))
 			return
-                # print (len(all_words))
-                # print (random.randint(0, len(all_words)-1))
-                # print (all_words[61405])
-                # return
+
+	def check_word_in_guessed_words(self, random_word):
+
+		guessed_words = 'guessedwords.dat'
+
+		try:
+			with open(guessed_words, 'r') as fp:
+				all_words = fp.readlines()
+				all_words = [word[:-1] for word in all_words]
+				if (random_word in all_words):
+					return True
+				return False
+		except IOError:
+			print ("\nCould not open the file {}\n".format(guessed_words))
+			return
+    
+	def start_game(self):
+    	
+    		random_word = self.load_random_word()
+    		word_size = len(random_word)
+    		
+    		print (len(random_word))
+
+    		self.current_word = random_word
+    		print(random_word)
+    		self.pattern = list(word_size*'_')[:-1]
+    		print (len(self.pattern))
+    		
+    		while(True):
+    			print ("\n")
+    			self.display_word(self.pattern)
+    			letter = raw_input("\nEnter a letter: ")
+    			self.check_letter_in_word(letter, random_word)	
+    			print ("\n")	
+
+	def check_letter_in_word(self, letter, word):
+    		
+    		if letter in word:
+    			self.replace_letter_in_word(letter, word)
+
+
+	def replace_letter_in_word(self, letter, word):	
+		
+		for index in range(0, len(word)):
+				if letter == word[index]:
+					self.pattern[index] = letter    			
+
+
+
+	def display_word(self, pattern):
+    		
+   			#pattern_size = len(pattern)
+   			print (self.pattern)
+   			print (''.join(self.pattern))
+   			
+   			# for i in range(1, pattern_size):
+   			# 	underscore += '_'
+   			# print (underscore)
 
 
 
@@ -65,7 +118,7 @@ def main():
 
 	game = Game()
 	game.display_menu()
-	#game.load_random_word()
+	
 
 if __name__ == "__main__":
 	main()	
