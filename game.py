44ce6a5de6b85eaf81186cf1679955d2eef707d1
pgenev <philip_genev@abv.bg>
2016-12-04 21:42:51 +0200

    progress

--- game.py
+++ game.py
@@ -6,6 +6,8 @@ class Game:
 	def __init__(self):
 		self.current_word = ""
 		self.pattern = []
+		self.used_letters = []
+		self.next_picture = 0
 
 	def display_menu(self):
 
@@ -45,7 +47,6 @@ class Game:
 					while(True):
 						random_word = all_words[random.randint(0, len(all_words)-1)]
 						if ((self.check_word_in_guessed_words(random_word) == True) or (len(random_word) < 4)):
-							random_word = all_words[random.randint(0, len(all_words)-1)]
 							continue
 						break
 				return random_word
@@ -69,48 +70,79 @@ class Game:
 			return
     
 	def start_game(self):
-    	
+    		
     		random_word = self.load_random_word()
+    		print (random_word)
+    		random_word = random_word.lower()
     		word_size = len(random_word)
-    		
-    		print (len(random_word))
-
     		self.current_word = random_word
-    		print(random_word)
-    		self.pattern = list(word_size*'_')[:-1]
-    		print (len(self.pattern))
+    		self.pattern = list(word_size*'_')
     		
+    		#print ("random_word length {}".format(len(random_word)))
+    		#print(random_word)
+    		#print ("pattern length {}".format(len(self.pattern)))
+    	
     		while(True):
     			print ("\n")
-    			self.display_word(self.pattern)
-    			letter = raw_input("\nEnter a letter: ")
-    			self.check_letter_in_word(letter, random_word)	
+    			self.display_word()
+    			letter = input("\nEnter a letter: ")
+    			letter = letter.lower()
+    			self.display_gallows()
+    			if (self.check_letter_in_word(letter) == True):
+    				self.replace_letter_in_word(letter)
+    				self.used_letters.append(letter)
     			print ("\n")	
 
-	def check_letter_in_word(self, letter, word):
+	def check_letter_in_word(self, letter):
     		
-    		if letter in word:
-    			self.replace_letter_in_word(letter, word)
+    		if letter in self.current_word:
+    			return True
+    		return False
 
 
-	def replace_letter_in_word(self, letter, word):	
+	def replace_letter_in_word(self, letter):	
 		
-		for index in range(0, len(word)):
-				if letter == word[index]:
+		for index in range(0, len(self.current_word)):
+				if letter == self.current_word[index]:
 					self.pattern[index] = letter    			
 
 
 
-	def display_word(self, pattern):
-    		
-   			#pattern_size = len(pattern)
-   			print (self.pattern)
-   			print (''.join(self.pattern))
-   			
-   			# for i in range(1, pattern_size):
-   			# 	underscore += '_'
-   			# print (underscore)
+	def display_word(self):
 
+			print (self.pattern)
+			print (' '.join(self.pattern))
+   
+	def load_and_reverse_gallows(self):
+
+		gallows = 'gallows.dat'
+
+		try:
+			with open(gallows, 'r') as fp:
+				gallows = fp.readlines()
+				for line in gallows:
+					line = line[:-1]
+					mirror_line = list(line[::-1])
+
+					for char in range(0, len(mirror_line)-1):
+							if '/' == mirror_line[char]:
+								mirror_line[char] = '\\'
+							elif('\\' == mirror_line[char]):
+								mirror_line[char] = '/'
+					#print ("{}".format(''.join(gallows_miror)))
+					yield ''.join(mirror_line)
+		except IOError:
+			print ("\nCould not open the file {}\n".format(gallows))
+			return
+   	
+	def display_gallows(self):
+   		generator = self.load_and_reverse_gallows()
+   		self.next_picture += 7
+   		print(self.next_picture)
+   		for i in range(0, self.next_picture):
+   			print (next(generator))
+   		
+   		#print (','.join(current_line))
 
 
 
@@ -118,6 +150,7 @@ def main():
 
 	game = Game()
 	game.display_menu()
+
 	
 
 if __name__ == "__main__":
